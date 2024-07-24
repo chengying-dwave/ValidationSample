9484c44e3493888ca3745ecff3ad20a5fc033a58
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-24 16:58:53 +0800

    feat: add ValidationUsingDataAnnotationViewModel

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,4 +5,9 @@ public partial class MainWindowViewModel : ViewModelBase
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+
+    /// <summary>
+    /// Gets a ViewModel showing how to use DataAnnotations for validation
+    /// </summary>
+    public ValidationUsingDataAnnotationViewModel ValidationUsingDataAnnotationViewModel { get;  } = new ValidationUsingDataAnnotationViewModel();
 }
