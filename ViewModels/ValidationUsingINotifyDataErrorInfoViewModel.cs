4eafa5f85a06381b2fa147e0416607a2bce0dd86
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-25 15:15:04 +0800

    feat: implement INotifyDataErrorInfo

--- ViewModels/ValidationUsingINotifyDataErrorInfoViewModel.cs
+++ ViewModels/ValidationUsingINotifyDataErrorInfoViewModel.cs
@@ -1,5 +1,39 @@
-﻿namespace ValidationSample.ViewModels;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
-public class ValidationUsingINotifyDataErrorInfoViewModel : ViewModelBase
+namespace ValidationSample.ViewModels;
+
+public class ValidationUsingINotifyDataErrorInfoViewModel : ViewModelBase, INotifyDataErrorInfo
 {
+    // Store Errors in a Dictionary
+    private readonly Dictionary<string, List<ValidationResult>> errors = [];
+
+    // we have errors present if errors.Count is greater than 0
+    public bool HasErrors => errors.Count > 0;
+
+    // Implement members of INotifyDataErrorInfo
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+    /// <inheritdoc/>
+    public IEnumerable GetErrors(string? propertyName)
+    {
+        // Get entity-level errors when the target property is null or empty
+        if (string.IsNullOrEmpty(propertyName))
+        {
+            return errors.Values.SelectMany(static errors => errors);
+        }
+
+        // Property-level errors, if any
+        if (errors.TryGetValue(propertyName!, out List<ValidationResult>? result))
+        {
+            return result;
+        }
+
+        // In case there are no errors we return an empty array.
+        return Array.Empty<ValidationResult>();
+    }
 }
