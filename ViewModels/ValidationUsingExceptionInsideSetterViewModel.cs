44f071e11e562f5b4191ef7c59ae5b291c88c383
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-25 16:42:10 +0800

    feat: add Email

--- ViewModels/ValidationUsingExceptionInsideSetterViewModel.cs
+++ ViewModels/ValidationUsingExceptionInsideSetterViewModel.cs
@@ -1,5 +1,35 @@
-﻿namespace ValidationSample.ViewModels;
+﻿using ReactiveUI;
+using System;
+
+namespace ValidationSample.ViewModels;
 
 public class ValidationUsingExceptionInsideSetterViewModel : ViewModelBase
 {
+    private string? _email;
+
+    /// <summary>
+    /// Validation using Exceptions (only inside setter allowed!)
+    /// </summary>
+    public string? Email
+    {
+        get { return _email; }
+        set
+        {
+            // The field may not be null or empty
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(Email), "This field is required");
+            }
+            // The field must contain an '@' sign
+            else if (!value.Contains('@'))
+            {
+                throw new ArgumentException("NOt a valid E-Mail-Address", nameof(Email));
+            }
+            // The checks were successful, so we can store the value
+            else
+            {
+                this.RaiseAndSetIfChanged(ref _email, value);
+            }
+        }
+    }
 }
